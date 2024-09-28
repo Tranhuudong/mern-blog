8fb955534a43444cc945ddb4ac7c76b229cae703
Tranhuudong <tranhuudong282001@gmail.com>
2024-09-28 22:59:22 +0700

    create header componnet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,18 @@ import Home from "./page/Home";
 import About from "./page/About";
 import SignIn from "./page/SignIn";
 import SignUp from "./page/SignUp";
-import Dashboard from "./page/Dashboard";
 import Projects from "./page/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
       </Routes>
     </BrowserRouter>
