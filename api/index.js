8990334184a9738e2ce2b467ea629cac9a3a3551
Tranhuudong <tranhuudong282001@gmail.com>
2024-09-29 00:13:04 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to Mongoose");
+  })
+  .catch(() => {
+    console.log(error);
+  });
 
-const app = express()
-app.listen(3000, ()=> {
-    console.log('server listening on port 3000');
-})
+const app = express();
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
