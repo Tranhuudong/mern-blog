87822e1e423292db691970c802e62005412775ce
Tranhuudong <tranhuudong282001@gmail.com>
2024-10-10 17:14:00 +0700

    update haeader search form funct

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -16,11 +35,16 @@ export default function Header() {
         </Link>
 
         {/* Search form section */}
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64 "
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
             <FaSearch className="text-slate-600" />
@@ -38,14 +62,18 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/profile">
-          <li className="flex items-center">
-            {currentUser ? (
-              <img src={currentUser.avatar} alt="profile" className="w-8 h-8 rounded-full" />
-            ) : (
-              <span className="hidden sm:inline text-slate-700 hover:underline">
-                Sign in
-              </span>
-            )}
+            <li className="flex items-center">
+              {currentUser ? (
+                <img
+                  src={currentUser.avatar}
+                  alt="profile"
+                  className="w-8 h-8 rounded-full"
+                />
+              ) : (
+                <span className="hidden sm:inline text-slate-700 hover:underline">
+                  Sign in
+                </span>
+              )}
             </li>
           </Link>
         </ul>
